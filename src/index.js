484a711b8388505d725541b290d704e145302da2
bao.nt <65020023+bao-nguyenbku@users.noreply.github.com>
2023-08-16 20:52:49 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import { registerRootComponent } from 'expo';
+import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import * as SplashScreen from 'expo-splash-screen';
+import { NativeBaseProvider } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { enableScreens } from 'react-native-screens';
+import App from '@/App';
+// import store from '';
+
+SplashScreen.preventAutoHideAsync();
+
+function Entry() {
+  enableScreens(false);
+  const [isReady, setIsReady] = useState(false);
+
+  setTimeout(() => {
+    SplashScreen.hideAsync().then(() => setIsReady(true));
+  }, 2000);
+
+  if (isReady) {
+    return (
+      <Provider store={store}>
+        <NavigationContainer>
+          <NativeBaseProvider>
+            <App />
+          </NativeBaseProvider>
+        </NavigationContainer>
+      </Provider>
+    );
+  }
+}
+
+
+registerRootComponent(Entry);
